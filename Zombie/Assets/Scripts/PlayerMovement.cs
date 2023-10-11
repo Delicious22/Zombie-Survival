2ae82a59bb6c278d5b633958733c953f09b3d264
Dongin <hdin7669@gmail.com>
2023-10-11 12:37:49 +0900

    [Update] 플레이어 캐릭터의 움직임을 변경
    플레이어 캐릭터의 움직이는 방식을
    캐릭터가 바라보는 방향으로 앞뒤로만 움직일 수 있던것에서
    화면기준 상하좌우로 움직이는 방식으로 변경

--- Zombie/Assets/Scripts/PlayerMovement.cs
+++ Zombie/Assets/Scripts/PlayerMovement.cs
@@ -25,13 +25,27 @@ public class PlayerMovement : MonoBehaviour {
 
         Move();
 
-        playerAnimator.SetFloat("Move" , playerInput.move);
+        float inputMove = playerInput.move + Mathf.Abs(playerInput.rotate);
+        if (playerInput.move < 0) inputMove = playerInput.move;
+        playerAnimator.SetFloat("Move" , inputMove);
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
     private void Move() {
-        Vector3 moveDistance = playerInput.move * transform.forward * moveSpeed * Time.deltaTime;
-        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance);
+        // 카메라의 z축방향과 x축방향을 구함
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+        // 카메라가 기울어져 있기 때문에 y를 0으로
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        // 카메라의 Vector를 정규화
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+
+        // 움직이는 Vector를 입력에 맞게 변화
+        Vector3 movement = cameraForward * playerInput.move + cameraRight * playerInput.rotate;
+        movement.Normalize();
+        playerRigidbody.MovePosition(playerRigidbody.position + (movement * moveSpeed * Time.deltaTime));
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
