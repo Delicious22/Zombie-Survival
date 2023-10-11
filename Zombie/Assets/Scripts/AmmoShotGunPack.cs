1ba76cd88ae3e0ada61c9cc1ddc55c93c2c4b12d
praylee0226 <praylee917537@gmail.com>
2023-10-11 13:17:24 +0900

    [Feat] New Script
    샷건팩 함수를 추가해서 ItemSpawner에 연동

---
+++ Zombie/Assets/Scripts/AmmoShotGunPack.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+// 총알을 충전하는 아이템
+public class AmmoShotGunPack : MonoBehaviour, IItem {
+    public int ammo = 2; // 충전할 총알 수
+
+    /// <summary>
+    /// 샷건은 플레이어의 해즈 웨폰과 웨폰에 추가되야함.
+    /// </summary>
+    public void Use(GameObject target) {
+        // 전달 받은 게임 오브젝트로부터 PlayerShooter 컴포넌트를 가져오기 시도
+        PlayerShooter playerShooter = target.GetComponent<PlayerShooter>();
+        PlayerSwap playerSwap = target.GetComponent<PlayerSwap>();
+        // PlayerShooter 컴포넌트가 있으며, 총 오브젝트가 존재하면
+        if (playerShooter != null && playerShooter.shotgun != null)
+        {
+            // 총의 남은 탄환 수를 ammo 만큼 더한다
+            playerShooter.shotgun.ammoRemain += ammo;
+        }
+        playerSwap.hasWeapons[1] = true;
+        // 사용되었으므로, 자신을 파괴
+        Destroy(gameObject);
+    }
+}
